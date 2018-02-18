d77418e980c229aa726ed3c7f76a42880c116222
Ilya Fedoseev <shift.of.razum@gmail.com>
2018-02-18 07:33:09 +0300

    Intents and Broadcast Receivers

---
+++ android-intents-broadcast-receivers/app/src/main/java/com/teamtreehouse/musicmachine/DetailActivity.java
@@ -0,0 +1,38 @@
+package com.teamtreehouse.musicmachine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.teamtreehouse.musicmachine.models.Song;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private Song mSong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        TextView titleLabel = (TextView)findViewById(R.id.songTitleLabel);
+        final CheckBox favoriteCheckbox = (CheckBox)findViewById(R.id.checkBox);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
