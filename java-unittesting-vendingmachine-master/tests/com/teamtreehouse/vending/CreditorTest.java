25fd8c4b2611031e12ea516465b4ceb2d192d8b9
Ilya Fedoseev <shift.of.razum@gmail.com>
2018-02-25 12:24:47 +0300

    Why Test? - Introduction - Defining a Unit - Introducing the Project - Code Reuse - The Quick Prototype

---
+++ java-unittesting-vendingmachine-master/tests/com/teamtreehouse/vending/CreditorTest.java
@@ -0,0 +1,41 @@
+package com.teamtreehouse.vending;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreditorTest {
+
+    private Creditor creditor;
+
+    @Before
+    public void setUp() throws Exception {
+        creditor = new Creditor();
+    }
+
+    @Test
+    public void addingFundsIncrementsAvailableFunds() throws Exception {
+        creditor.addFunds(25);
+        creditor.addFunds(25);
+
+        assertEquals(50, creditor.getAvailableFunds());
+    }
+
+    @Test
+    public void refundingReturnsAllAvailableFunds() throws Exception {
+        creditor.addFunds(10);
+        int refund =  creditor.refund();
+
+        assertEquals(10, refund);
+
+    }
+
+    @Test
+    public void refundingResetsAvailableFundsToZero() throws Exception {
+        creditor.addFunds(10);
+        creditor.refund();
+
+        assertEquals(0, creditor.getAvailableFunds());
+    }
+}
