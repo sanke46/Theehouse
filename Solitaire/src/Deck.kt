b74234db8c6f1adb77f0f67e99b8d1a8403027ff
Ilya Fedoseev <shift.of.razum@gmail.com>
2018-02-28 12:45:53 +0300

    Hello Kotlin! - Introducing Kotlin and the Project - Creating the Project - Creating the Card Class - The Card Class: Kollector's Edition - Finishing the Card Class - Making the Deck - Stacking the Deck - Compacting the Deck - Using the Deck

---
+++ Solitaire/src/Deck.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+class Deck {
+
+    val cards = Array(52, { Card(it % 13, getSuit(it))})
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawcard() : Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck = cards.toMutableList()
+        Collections.shuffle(cardsInDeck)
+    }
+
+    private fun getSuit(i: Int) = when(i / 13) {
+        0 -> "Clubs"
+        1 -> "Diamonds"
+        2 -> "Hearts"
+        else -> "Spades"
+    }
+}
